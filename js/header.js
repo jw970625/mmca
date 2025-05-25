c0b2c9c5e6ca49e0bbae595dc14309ca9ca0de3b
jw970625 <parkjeongwoo970625@gmail.com>
2025-05-25 21:01:50 +0900

    first

---
+++ js/header.js
@@ -0,0 +1,43 @@
+
+
+/* Desktop Header */
+$(document).ready(function () {
+
+  $(".sub").hide();
+  $(".bg_box").hide();
+
+  $(function () {
+  $(".main").mouseenter(function () {
+    $(this).find(".sub").stop().slideDown(200);
+    $(".bg_box").stop().slideDown(200);
+  });
+  
+  $(".main").mouseleave(function () {
+    $(this).find(".sub").stop().slideUp(200);
+    $(".bg_box").stop().slideUp(200);
+  });
+});
+});
+
+/* Tab Header */
+$(".trigger").click(function () {
+  const $menu = $(".menu_all");
+
+  if ($menu.hasClass("active")) {
+    // 메뉴가 열려 있으면 닫기
+    $menu.stop(true, true).animate({ right: "-100%" });
+    $menu.removeClass("active");
+  } else {
+    // 메뉴가 닫혀 있으면 열기
+    $menu.stop(true, true).animate({ right: "0" });
+    $menu.addClass("active");
+  }
+  $(".tmain").click(function(){
+    $(this).siblings().find(".tsub").slideUp(300);
+    $(this).siblings().find(">a").removeClass("uparrow")
+    $(this).find(".tsub").stop().slideToggle(200);
+    $(this).find(">a").toggleClass("uparrow");
+  });
+});
+
+  
