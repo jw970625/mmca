95103276dbdc627bac130d226237e68f4ed4ddf6
jw970625 <parkjeongwoo970625@gmail.com>
2025-06-04 23:01:15 +0900

    333
    333

--- js/shop.js
+++ js/shop.js
@@ -5,26 +5,23 @@ $(document).ready(function(){
       fadeIndicator = fadeContainer.find(".fade_indicator a"),
       fadePrev = fadeContainer.find(".fade_side .prev"),
       fadeNext = fadeContainer.find(".fade_side .next"),
-      oldidx = 0,  //기존이미지 index
-      idx=0,  //새로 바꾸는 이미지 index
-      fadeCount = fadeImage.length, //length메서드:1,2,3....개수를 세어줌
+      oldidx = 0,
+      idx=0,
+      fadeCount = fadeImage.length, 
       interval = 4000;
 
-  function fadeAni(idx){ //매개변수가 있는 함수 ->idx는 선택되는 이미지
-    if(oldidx != idx){ //기존의 이미지와 선택된 이미지가 다를때...
-      fadeIndicator.eq(oldidx).removeClass("active"); //기존 하단버튼 비활성화 ->active 클래스를 삭제
-      fadeIndicator.eq(idx).addClass("active"); //선택된 하단버튼 활성화 ->active 클래스를 추가\
-      fadeImage.eq(oldidx).stop().fadeOut("400"); //기존 이미지 사라짐
-      fadeImage.eq(idx).stop().fadeIn("400"); //새로 바뀌는 이미지 나타남  
+  function fadeAni(idx){ 
+    if(oldidx != idx){
+      fadeIndicator.eq(oldidx).removeClass("active");
+      fadeIndicator.eq(idx).addClass("active");
+      fadeImage.eq(oldidx).stop().fadeOut("400");
+      fadeImage.eq(idx).stop().fadeIn("400");
     };
-    oldidx = idx; //새로 바뀐 이미지는 이미지는 다시 기존이미지로 저장되어서 Fade Out...
+    oldidx = idx;
   };
 
-  //자동Fade함수
-  //일정시간마다 할일: setInterval(함수명,시간) / clearInterval(변수) 
   function fadeAuto(){
     fadeTimer = setInterval(function(){
-      //현재가 (0+1)%5=1,(1+1)%5=2,(2+1)%5=3, (3+1)%5=4, (4+1)%5=0   
       idx = (oldidx + 1) % fadeCount;
       fadeAni(idx);
     },interval);
@@ -40,20 +37,19 @@ $(document).ready(function(){
   //좌우버튼
   fadePrev.click(function(){
     idx--;
-    if(idx < 0){ //선택한 이미지가 4,3,2,1,0 첫번째일때 맨 마지막부터 다시 시작
-      idx = fadeCount-1; //fadeCount는 length로 센 개수 5에서 1을 빼야 마지막 index 4가 됨
+    if(idx < 0){ 
+      idx = fadeCount-1;됨
     }
     fadeAni(idx);
   });
   fadeNext.click(function(){
     idx++;
-    if(idx > fadeCount-1){ //선택한 이미지가 0,1,2,3,4 마지막번째일때 맨 처음부터 다시 시작
+    if(idx > fadeCount-1){
       idx=0; 
     }
     fadeAni(idx);
   });
 
-  //전체영역에 마우스를 올리면 자동함수 멈추고 다시 마우스가 나오면 자동함수 실행
   fadeContainer.mouseenter(function(){
     clearInterval(fadeTimer);
   })
@@ -61,3 +57,4 @@ $(document).ready(function(){
     fadeAuto();
   });
 });
+
