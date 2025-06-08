7119919f9682d9cfdcbaf9ad2b60028b4a7d7e4d
jw970625 <parkjeongwoo970625@gmail.com>
2025-06-09 08:59:54 +0900

    js 오류 수정
    1

--- js/header.js
+++ js/header.js
@@ -24,7 +24,6 @@ $(document).ready(function () {
       $menu.stop(true, true).animate({ right: "-100%" });
       $menu.removeClass("active");
 
-      // 서브메뉴 초기화
       $(".tsub").slideUp(300);
       $(".tmain > a").removeClass("uparrow");
     } else {
@@ -33,14 +32,36 @@ $(document).ready(function () {
     }
   });
 
+  $(".tsub a").click(function (e) {
+    const href = $(this).attr('href');
+    if (href && href !== '#' && href.trim() !== '') {
+      return true;
+    }
+  });
+
   $(".tmain").click(function (e) {
-    e.preventDefault();
+    const $clickedElement = $(e.target);
+    
+    if ($clickedElement.closest('.tsub').length > 0) {
+      return;
+    }
 
-    $(this).siblings().find(".tsub").slideUp(300);
-    $(this).siblings().find(">a").removeClass("uparrow");
+    if ($(this).find(".tsub").length > 0) {
+      e.preventDefault();
+      
+      $(this).siblings().find(".tsub").slideUp(300);
+      $(this).siblings().find(">a").removeClass("uparrow");
 
-    $(this).find(".tsub").stop().slideToggle(200);
-    $(this).find(">a").toggleClass("uparrow");
+      $(this).find(".tsub").stop().slideToggle(200);
+      $(this).find(">a").toggleClass("uparrow");
+    }
+  });
+
+  $(".tab_header_left a, .tab_header_logo a, .menu_user a").click(function (e) {
+    const href = $(this).attr('href');
+    if (href && href !== '#' && href.trim() !== '') {
+      return true;
+    }
   });
 });
 
