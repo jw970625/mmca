76eb16a15540fd0eaec9ca08b3fb3b923d12281f
jw970625 <parkjeongwoo970625@gmail.com>
2025-06-08 19:01:50 +0900

    tt
    ttt

--- js/footer.js
+++ js/footer.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   $(".title_footer").click(function(){
     $(this).toggleClass("active");
-    $(".sub-navi_footer").slideToggle(200);
+    $(".sub-navi_footer").slideToggle(50);
   });
 
 });
